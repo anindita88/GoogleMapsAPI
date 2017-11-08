2a6a131e6fd9857b154dd2945eb93252474a25db
Anindita <anindita.ekasuci88@gmail.com>
2017-11-09 04:16:03 +0700

    Quiz : Hometwon

--- app/src/main/java/id/sch/smktelkom_mlg/gmaps/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/gmaps/MainActivity.java
@@ -1,24 +1,14 @@
 package id.sch.smktelkom_mlg.gmaps;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
 import com.google.android.gms.maps.StreetViewPanorama;
-import com.google.android.gms.maps.StreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
 
 public class MainActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        StreetViewPanoramaFragment streetViewPanoramaFragment = (StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.streetviewpanorama);
-        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
-
-    }
 
     @Override
     public void onStreetViewPanoramaReady(StreetViewPanorama panorama) {
